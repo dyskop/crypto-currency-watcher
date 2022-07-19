a983a35a87a925341092c3b8b654595afeb6a130
Dzmitry Skopinau <dimaskop18@gmail.com>
2022-07-19 03:26:20 +0300

    fix: change RestTemplate to WebClient

--- src/main/java/by/skopinau/cryptocurrencywatcher/config/AppConfig.java
+++ src/main/java/by/skopinau/cryptocurrencywatcher/config/AppConfig.java
@@ -2,16 +2,21 @@ package by.skopinau.cryptocurrencywatcher.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
     public static final long TIME_INTERVAL = 60000;
-    public static final String URL = "https://api.coinlore.net/api/ticker/?id=%d";
+    public static final String BASE_URL = "https://api.coinlore.net";
 
-    // TODO: change to WebClient
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient webClient()
+    {
+        return WebClient.builder()
+                .baseUrl(BASE_URL)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 }
--- src/main/java/by/skopinau/cryptocurrencywatcher/service/util/CurrencyClient.java
+++ src/main/java/by/skopinau/cryptocurrencywatcher/service/util/CurrencyClient.java
@@ -1,25 +1,33 @@
 package by.skopinau.cryptocurrencywatcher.service.util;
 
-import by.skopinau.cryptocurrencywatcher.config.AppConfig;
 import by.skopinau.cryptocurrencywatcher.dal.entity.Currency;
 import by.skopinau.cryptocurrencywatcher.dto.CurrencyRequest;
 import by.skopinau.cryptocurrencywatcher.mapper.CurrencyMapper;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class CurrencyClient {
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    public CurrencyClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public CurrencyClient(WebClient webClient) {
+        this.webClient = webClient;
     }
 
     public Currency getActualCurrency(long id) {
-        CurrencyRequest[] template = null;
-        while (template == null) {
-            template = restTemplate.getForObject(String.format(AppConfig.URL, id), CurrencyRequest[].class);
+        CurrencyRequest[] currencyRequests = null;
+
+        while (currencyRequests == null) {
+            currencyRequests = webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/api/ticker/")
+                            .queryParam("id", id)
+                            .build())
+                    .retrieve()
+                    .bodyToMono(CurrencyRequest[].class)
+                    .block();
         }
-        return CurrencyMapper.INSTANCE.currencyRequestToCurrencyEntity(template[0]);
+
+        return CurrencyMapper.INSTANCE.currencyRequestToCurrencyEntity(currencyRequests[0]);
     }
 }
